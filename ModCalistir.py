6b6e1f118a0d309d0fc3c6542e4ce83212c0e070
İbrahim EDİZ <KURS>
2019-11-20 19:59:26 +0200

    20112019_5

--- ModCalistir.py
+++ ModCalistir.py
@@ -1,3 +1,25 @@
-from MODUL.DosyaModul import *
+# import os
+# import datetime
+# import time
+#built in modules
+# print(os.name)
+# print(os.getcwd())
+# print(os.sep)
+# print(os.makedirs(r"Ali\Veli\Hakkı"))
+# import platform as plt
+# print(plt.python_build())
+from random import randrange,random,choice,sample,seed
 
-dosya = dosyaAc()
+# liste = [i for i in range(1,50)]
+# ornek = sample(liste,6)
+# ornek.sort()
+# print(ornek)
+seed(2)
+print("1",random())
+print("2",random())
+print("3",random())
+print("4",random())
+print("5",random())
+
+from math import factorial
+print(factorial(5))
