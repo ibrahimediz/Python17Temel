0f6bd327b9187768cdbeb91de5d1ad55b86a690b
İbrahim EDİZ <KURS>
2019-11-20 17:53:14 +0200

    20112019

--- Fonksiyonlar5.py
+++ Fonksiyonlar5.py
@@ -30,3 +30,13 @@
 # print(siralama)
 
 
+
+def fib(n):
+    if n < 1:
+        return None
+    if n < 3:
+        return 1
+    return fib(n - 1) + fib(n - 2)
+
+for n in range(1, 20):
+    print(n, "->", fib(n))
