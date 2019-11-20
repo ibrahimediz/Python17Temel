bc0c81cd4f2cf04694963829bf1d352c1503bbf4
İbrahim EDİZ <KURS>
2019-11-20 18:43:29 +0200

    20112019_2

--- MODUL/DosyaModul.py
+++ MODUL/DosyaModul.py
@@ -19,15 +19,42 @@ def Ekleme(adi="-",soyadi="-",tel="0"):
         dosya.seek(0)
         dosya.truncate()
         dosya.writelines(liste)
+        return True
+    except Exception as hata:
+        print("Hata Var :",hata)
+        return False
+    finally:
+        dosya.close()
+# Ekleme("ali","veli","4950")
+def Listele():
+    try :
+        dosya = dosyaAc()
+        liste = dosya.readlines()
+        sira = 1
+        for item in liste:
+            kayit = item.replace(";","\t")
+            print(sira,kayit,sep="-")
+            sira += 1
     except Exception as hata:
         print("Hata Var :",hata)
     finally:
         dosya.close()
-# Ekleme("ali","veli","4950")
-dosya = dosyaAc()
-liste = dosya.readlines()
-sira = 1
-for item in liste:
-    kayit = item.replace(";","\t")
-    print("{}-{}".format(sira,kayit))
-    sira += 1
+
+def Guncelleme(adi="-",soyadi="-",tel="0"):
+    try:
+        dosya = dosyaAc()
+        liste = dosya.readlines()
+        if not tel.isnumeric():
+            tel = 0
+        kayit = "{};{};{}\n".format(adi,\
+        soyadi,tel)
+        liste.append(kayit)
+        dosya.seek(0)
+        dosya.truncate()
+        dosya.writelines(liste)
+        return True
+    except Exception as hata:
+        print("Hata Var :",hata)
+        return False
+    finally:
+        dosya.close()
