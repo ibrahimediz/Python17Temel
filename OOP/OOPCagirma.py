72f0a239440c8a9fa62a694073b77a23f11464cd
İbrahim EDİZ <ibrahim.ediz@gazi.edu.tr>
2019-12-06 18:06:03 +0200

    06122019

---
+++ OOP/OOPCagirma.py
@@ -0,0 +1,34 @@
+from OOPOrnek1 import Deadpool,Hulk,IronMan,Tarkan
+from random import choice
+import time
+karliste = [Deadpool,Hulk,IronMan,Tarkan]
+p1 = choice(karliste)()
+p2 = choice(karliste)()
+
+while p1.saglik > 0 and p2.saglik >0:
+    time.sleep(1)
+    p1.DarbeAlGetir()(p2.VurusGetir()())
+    p2.DarbeAlGetir()(p1.VurusGetir()())
+    print(p1.Durum(),"-",p2.Durum())
+else:
+    if p1.saglik > p2.saglik:
+        print(p1.adi," Kazandı")
+    elif p2.saglik > p1.saglik:
+        print(p2.adi," Kazandı")
+    else:
+        print("Berabere")
+p1.BirikimAyarla(3)
+
+# while p1.saglik > 0 and p2.saglik >0:
+#     time.sleep(1)
+#     p1.DarbeAl(p2.Vurus())
+#     p2.DarbeAl(p1.Vurus())
+#     print(p1.Durum(),"-",p2.Durum())
+#     print(p1.BirikimGetir())
+# else:
+#     if p1.saglik > p2.saglik:
+#         print(p1.adi," Kazandı")
+#     elif p2.saglik > p1.saglik:
+#         print(p2.adi," Kazandı")
+#     else:
+#         print("Berabere")
---
+++ OOP/OOPOrnek1.py
@@ -0,0 +1,83 @@
+from random import choice
+class MarvelHero:
+    def __init__(self,guc,saglik,adi,superGuc):
+        self.guc = guc
+        self.MaxSaglik = saglik
+        self.saglik = saglik
+        self.adi = adi
+        self.superGuc = superGuc
+        self.__gucBirikim = 0
+    def Vurus(self):
+        self.BirikimAyarla(1)
+        return self.guc
+    def DarbeAl(self,darbe):
+        self.saglik -= darbe
+    def Savunma(self,darbe):
+        print(self.adi,"Savunma Yaptı")
+        self.saglik -= darbe//2
+    def SuperVurus(self):
+        if self.__gucBirikim >= 4:
+            self.__gucBirikim = 0
+            print(self.adi,"Süper Vuruş Yaptı")
+            return self.guc*self.superGuc
+        else:
+            return self.Vurus()
+
+    def BirikimAyarla(self,deger): # setter
+        self.__gucBirikim = (self.__gucBirikim + deger )% 5
+
+
+    def VurusGetir(self):
+        hareket = [self.Vurus,self.SuperVurus]
+        return choice(hareket)
+
+    def DarbeAlGetir(self):
+        hareket = [self.DarbeAl,self.Savunma]
+        return choice(hareket)
+
+    def BirikimGetir(self): ## getter
+        return self.__gucBirikim
+
+    def Durum(self):
+        bar = (self.saglik*100)/self.MaxSaglik
+        barPer =bar//10
+        return "Durum {} - Sağlık:{}%{}".format(self.adi,
+        int(barPer)*"#",round(bar))
+    
+    def __del__(self):
+        print("Hoşçakal")
+
+# P1 = MarvelHero(100,1000,"Deadpool")
+        
+class Deadpool(MarvelHero):
+    def __init__(self):
+        super().__init__(100,1000,"Deadpool",2)
+
+    def DarbeAl(self,darbe):
+        self.saglik -= darbe//2    
+
+class Hulk(MarvelHero):
+    def __init__(self):
+        super().__init__(100,2000,"Hulk",1.5)
+    
+    def Vurus(self):
+        self.BirikimAyarla(1)
+        return self.guc*2
+
+class IronMan(MarvelHero):
+    def __init__(self):
+        super().__init__(150,1500,"IronMan",3)
+
+class Tarkan(MarvelHero):
+    def __init__(self):
+        super().__init__(200,1500,"Tarkan",1.25)
+
+    def DarbeAl(self,darbe):
+        self.saglik -= darbe//3
+
+    def Vurus(self):
+        self.BirikimAyarla(1)
+        return self.guc//2
+
+
+
